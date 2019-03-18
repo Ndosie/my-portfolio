86b55d735f8bc85b3809fc1ce35d475d62fe6db7
Esther Ndosi <estherndosi24@gmail.com>
2019-03-18 13:58:07 +0300

    Updates

---
+++ precache-manifest.fcf0ddaf9d0a265575c3221d11406e8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4d95a87c7a02a69aacb",
+    "url": "/my-portfolio/static/js/runtime~main.d4d95a87.js"
+  },
+  {
+    "revision": "b334a0b4cac7212615b4",
+    "url": "/my-portfolio/static/js/main.b334a0b4.chunk.js"
+  },
+  {
+    "revision": "f3de22af7d8316bdd62c",
+    "url": "/my-portfolio/static/js/2.f3de22af.chunk.js"
+  },
+  {
+    "revision": "b334a0b4cac7212615b4",
+    "url": "/my-portfolio/static/css/main.45638661.chunk.css"
+  },
+  {
+    "revision": "b17a378251665fec033044cc5efc2b55",
+    "url": "/my-portfolio/index.html"
+  }
+];
